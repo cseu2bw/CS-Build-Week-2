86375c74718e18ac8746f08534728f24ead03dd2
krikaz <pascal.bou@protonmail.com>
2019-11-19 10:46:06 +0100

    added take

---
+++ src/actions.py
@@ -0,0 +1,32 @@
+import os
+import threading
+import time
+from room import Room
+from util import Queue
+
+base_url = os.environ['BASE_URL']
+token  = os.environ['TOKEN']
+
+if token == '' or token is None:
+    print('Invalid token')
+
+class Actions:
+    def __init__(self):
+        self.token = 'Token ' + token
+        self.base_url = base_url
+        self.next_action_time = time.time()
+        self.current_room = Room()
+        self.queue = Queue()
+
+    def take(self, item):
+        response = requests.post(self.base_url + '/adv/take/', headers={'Authorization': self.token}, json={'name': item})
+        try:
+            data = response.json()
+        except ValueError:
+            print("Error:  Non-json response")
+            print("Response returned:")
+            print(response)
+            return
+        self.next_action_time = time.time() + float(data.get('cooldown'))
+        self.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get('description'), data.get('coordinates'), data.get('elevation'), data.get('terrain'), data.get('items'))
+        print("Respose:", data)

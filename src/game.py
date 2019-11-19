1e250b53bf3003922fe0c1bc2d35d2fb61e84ca6
GianBarGian <giacomobenati@hotmail.it>
2019-11-19 11:04:31 +0100

    added find to path method for game.py

---
+++ src/game.py
@@ -0,0 +1,30 @@
+from util import Stack, Queue
+
+class Game:
+    def find_path_to(self, starting_room_id, target_id, adj):
+        queue = Queue()
+        final_path = None
+        visited = set()
+        queue.enqueue([starting_room_id])
+        while queue.len() > 0:
+            path = queue.dequeue()
+            vert = path[-1]
+            if vert not in visited:
+                if vert == target_id:
+                   final_path = path
+                   break
+                visited.add(vert)
+                for room in adj[vert].items():
+                    new_path = list(path)
+                    new_path.append(room)
+                    q.enqueue(new_path)
+        if final_path is None:
+            return
+        new_path = []
+        for idx, room in enumerate(final_path):
+            if idx > 0:
+                lastRoom = final_path[idx - 1]
+                for direction in adj[lastRoom]:
+                    if adj[lastRoom][direction] == room:
+                        new_path.append({'dir': direction, 'next_room': room})
+        return new_path

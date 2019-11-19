df6b94a52a3df0efd820523924c7642f6c00631a
krikaz <pascal.bou@protonmail.com>
2019-11-19 10:49:45 +0100

    added drop, check price, sell

--- src/actions.py
+++ src/actions.py
@@ -1,3 +1,4 @@
+import requests
 import os
 import threading
 import time
@@ -30,3 +31,42 @@ class Actions:
         self.next_action_time = time.time() + float(data.get('cooldown'))
         self.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get('description'), data.get('coordinates'), data.get('elevation'), data.get('terrain'), data.get('items'))
         print("Respose:", data)
+
+    def drop(self, item):
+        response = requests.post(self.base_url + '/adv/drop/', headers={'Authorization': self.token}, json={'name': item})
+        try:
+            data = response.json()
+        except ValueError:
+            print("Error:  Non-json response")
+            print("Response returned:")
+            print(response)
+            return
+        self.next_action_time = time.time() + float(data.get('cooldown'))
+        self.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get('description'), data.get('coordinates'), data.get('elevation'), data.get('terrain'), data.get('items'))
+        print("Respose:", data)        
+
+    def check_price(self, item):
+        response = requests.post(self.base_url + '/adv/sell/', headers={'Authorization': self.token}, json={'name': item})
+        try:
+            data = response.json()
+        except ValueError:
+            print("Error:  Non-json response")
+            print("Response returned:")
+            print(response)
+            return
+        self.next_action_time = time.time() + float(data.get('cooldown'))
+        self.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get('description'), data.get('coordinates'), data.get('elevation'), data.get('terrain'), data.get('items'))
+        print("Respose:", data)  
+
+    def sell(self, item):
+        response = requests.post(self.base_url + '/adv/sell/', headers={'Authorization': self.token}, json={'name': item, 'confirm': 'yes'})
+        try:
+            data = response.json()
+        except ValueError:
+            print("Error:  Non-json response")
+            print("Response returned:")
+            print(response)
+            return
+        self.next_action_time = time.time() + float(data.get('cooldown'))
+        self.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get('description'), data.get('coordinates'), data.get('elevation'), data.get('terrain'), data.get('items'))
+        print("Respose:", data)                

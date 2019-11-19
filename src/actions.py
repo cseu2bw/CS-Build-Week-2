ec5919f42551f431a35d9f141ab2092b5c9b4d92
krikaz <pascal.bou@protonmail.com>
2019-11-19 11:53:40 +0100

    refactored actions.py

--- src/actions.py
+++ src/actions.py
@@ -2,31 +2,24 @@ import requests
 import os
 import threading
 import time
+from player import Player
 from room import Room
 from util import Queue
 from status import Status
 
 base_url = os.environ['BASE_URL']
-token = os.environ['TOKEN']
-
-if token == '' or token is None:
-    print('Invalid token')
-
 
 class Actions:
-    def __init__(self):
-        self.token = 'Token ' + token
+    def __init__(self, player):
+        self.player = player
         self.base_url = base_url
-        self.next_action_time = time.time()
-        self.current_room = Room()
-        self.queue = Queue()
         self.message = ''
         self.status = Status()
         # self.other_player = Status()
 
     def take(self, item):
         response = requests.post(self.base_url + '/adv/take/',
-                                 headers={'Authorization': self.token}, json={'name': item})
+                                 headers={'Authorization': self.player.token}, json={'name': item})
         try:
             data = response.json()
         except ValueError:
@@ -34,14 +27,14 @@ class Actions:
             print("Response returned:")
             print(response)
             return
-        self.next_action_time = time.time() + float(data.get('cooldown'))
-        self.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get(
+        self.player.next_action_time = time.time() + float(data.get('cooldown'))
+        self.player.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get(
             'description'), data.get('coordinates'), data.get('elevation'), data.get('terrain'), data.get('items'))
         print("Response:", data)
 
     def drop(self, item):
         response = requests.post(self.base_url + '/adv/drop/',
-                                 headers={'Authorization': self.token}, json={'name': item})
+                                 headers={'Authorization': self.player.token}, json={'name': item})
         try:
             data = response.json()
         except ValueError:
@@ -49,14 +42,14 @@ class Actions:
             print("Response returned:")
             print(response)
             return
-        self.next_action_time = time.time() + float(data.get('cooldown'))
-        self.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get(
+        self.player.next_action_time = time.time() + float(data.get('cooldown'))
+        self.player.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get(
             'description'), data.get('coordinates'), data.get('elevation'), data.get('terrain'), data.get('items'))
         print("Response:", data)
 
     def check_price(self, item):
         response = requests.post(self.base_url + '/adv/sell/',
-                                 headers={'Authorization': self.token}, json={'name': item})
+                                 headers={'Authorization': self.player.token}, json={'name': item})
         try:
             data = response.json()
         except ValueError:
@@ -64,14 +57,14 @@ class Actions:
             print("Response returned:")
             print(response)
             return
-        self.next_action_time = time.time() + float(data.get('cooldown'))
-        self.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get(
+        self.player.next_action_time = time.time() + float(data.get('cooldown'))
+        self.player.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get(
             'description'), data.get('coordinates'), data.get('elevation'), data.get('terrain'), data.get('items'))
         print("Response:", data)
 
     def sell(self, item):
         response = requests.post(self.base_url + '/adv/sell/', headers={
-                                 'Authorization': self.token}, json={'name': item, 'confirm': 'yes'})
+                                 'Authorization': self.player.token}, json={'name': item, 'confirm': 'yes'})
         try:
             data = response.json()
         except ValueError:
@@ -79,14 +72,14 @@ class Actions:
             print("Response returned:")
             print(response)
             return
-        self.next_action_time = time.time() + float(data.get('cooldown'))
-        self.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get(
+        self.player.next_action_time = time.time() + float(data.get('cooldown'))
+        self.player.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get(
             'description'), data.get('coordinates'), data.get('elevation'), data.get('terrain'), data.get('items'))
         print("Response:", data)
 
     def check_status(self):
         response = requests.post(
-            self.base_url + '/adv/status/', headers={'Authorization': self.token})
+            self.base_url + '/adv/status/', headers={'Authorization': self.player.token})
         try:
             data = response.json()
         except ValueError:
@@ -94,13 +87,13 @@ class Actions:
             print("Response returned:")
             print(response)
             return
-        self.next_action_time = time.time() + float(data.get('cooldown'))
+        self.player.next_action_time = time.time() + float(data.get('cooldown'))
         self.status = Status(data.get('name'), data.get('cooldown'), data.get('encumbrance'), data.get('strength'), data.get('speed'), data.get('gold'), data.get('bodywear'), data.get('footwear'), data.get('inventory'), data.get('status'), data.get('errors'), data.get('messages'))
         print("Response:", data)
 
     def examine(self, item_or_player):
         response = requests.post(self.base_url + '/adv/examine/', headers={
-                                 'Authorization': self.token}, json={'name': item_or_player})
+                                 'Authorization': self.player.token}, json={'name': item_or_player})
         try:
             data = response.json()
         except ValueError:
@@ -108,13 +101,13 @@ class Actions:
             print("Response returned:")
             print(response)
             return
-        self.next_action_time = time.time() + float(data.get('cooldown'))
+        self.player.next_action_time = time.time() + float(data.get('cooldown'))
         # self.other_player = Status(data.get('name'), data.get('cooldown'), data.get('encumbrance'), data.get('strength'), data.get('speed'), data.get('gold'), data.get('bodywear'), data.get('footwear'), data.get('inventory'), data.get('status'), data.get('errors'), data.get('messages'))
         print("Response:", data)
 
     def wear(self, item):
         response = requests.post(self.base_url + '/adv/wear/',
-                                 headers={'Authorization': self.token}, json={'name': item})
+                                 headers={'Authorization': self.player.token}, json={'name': item})
         try:
             data = response.json()
         except ValueError:
@@ -122,13 +115,13 @@ class Actions:
             print("Response returned:")
             print(response)
             return
-        self.next_action_time = time.time() + float(data.get('cooldown'))
+        self.player.next_action_time = time.time() + float(data.get('cooldown'))
         self.status = Status(data.get('name'), data.get('cooldown'), data.get('encumbrance'), data.get('strength'), data.get('speed'), data.get('gold'), data.get('bodywear'), data.get('footwear'), data.get('inventory'), data.get('status'), data.get('errors'), data.get('messages'))
         print("Response:", data)
 
     def undress(self, item):
         response = requests.post(self.base_url + '/adv/undress/',
-                                 headers={'Authorization': self.token}, json={'name': item})
+                                 headers={'Authorization': self.player.token}, json={'name': item})
         try:
             data = response.json()
         except ValueError:
@@ -136,13 +129,13 @@ class Actions:
             print("Response returned:")
             print(response)
             return
-        self.next_action_time = time.time() + float(data.get('cooldown'))
+        self.player.next_action_time = time.time() + float(data.get('cooldown'))
         self.status = Status(data.get('name'), data.get('cooldown'), data.get('encumbrance'), data.get('strength'), data.get('speed'), data.get('gold'), data.get('bodywear'), data.get('footwear'), data.get('inventory'), data.get('status'), data.get('errors'), data.get('messages'))
         print("Response:", data)
 
     def change_name(self, new_name):
         response = requests.post(self.base_url + '/adv/change_name/',
-                                 headers={'Authorization': self.token}, json={'name': new_name})
+                                 headers={'Authorization': self.player.token}, json={'name': new_name})
         try:
             data = response.json()
         except ValueError:
@@ -150,13 +143,13 @@ class Actions:
             print("Response returned:")
             print(response)
             return
-        self.next_action_time = time.time() + float(data.get('cooldown'))
+        self.player.next_action_time = time.time() + float(data.get('cooldown'))
         self.status = Status(data.get('name'), data.get('cooldown'), data.get('encumbrance'), data.get('strength'), data.get('speed'), data.get('gold'), data.get('bodywear'), data.get('footwear'), data.get('inventory'), data.get('status'), data.get('errors'), data.get('messages'))
         print("Response:", data)
 
     def pray(self):
         response = requests.post(
-            self.base_url + '/adv/pray/', headers={'Authorization': self.token})
+            self.base_url + '/adv/pray/', headers={'Authorization': self.player.token})
         try:
             data = response.json()
         except ValueError:
@@ -164,16 +157,16 @@ class Actions:
             print("Response returned:")
             print(response)
             return
-        self.next_action_time = time.time() + float(data.get('cooldown'))
-        self.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get('description'), data.get('coordinates'), data.get('elevation'), data.get('terrain'), data.get('items'))
+        self.player.next_action_time = time.time() + float(data.get('cooldown'))
+        self.player.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get('description'), data.get('coordinates'), data.get('elevation'), data.get('terrain'), data.get('items'))
         print("Response:", data)
 
     def fly(self, dir):
-        if dir not in self.current_room.exits:
+        if dir not in self.player.current_room.exits:
             print('Invalid direction ' + dir)
             return
         to_send = {'direction': dir}
-        response = requests.post(self.base_url + '/adv/fly/', headers={'Authorization': self.token}, json=to_send)
+        response = requests.post(self.base_url + '/adv/fly/', headers={'Authorization': self.player.token}, json=to_send)
         try:
             data = response.json()
         except ValueError:
@@ -181,19 +174,19 @@ class Actions:
             print("Response returned:")
             print(response)
             return
-        self.next_action_time = time.time() + float(data.get('cooldown'))
-        self.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get('description'), data.get('coordinates'), data.get('elevation'), data.get('terrain'), data.get('items'))
+        self.player.next_action_time = time.time() + float(data.get('cooldown'))
+        self.player.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get('description'), data.get('coordinates'), data.get('elevation'), data.get('terrain'), data.get('items'))
         print("Respose:", data)                        
 
     def dash(self, dir, num_rooms, next_room_ids):
-        if dir not in self.current_room.exits:
+        if dir not in self.player.current_room.exits:
             print('Invalid direction ' + dir)
             return
         if int(num_rooms) != len(list(next_room_ids)):
             print(f'number of rooms do not match {num_rooms} {next_room_ids}')
             return    
         to_send = {'direction': dir, 'num_rooms': num_rooms, 'next_room_ids': next_room_ids}
-        response = requests.post(self.base_url + '/adv/dash/', headers={'Authorization': self.token}, json=to_send)
+        response = requests.post(self.base_url + '/adv/dash/', headers={'Authorization': self.player.token}, json=to_send)
         try:
             data = response.json()
         except ValueError:
@@ -201,13 +194,13 @@ class Actions:
             print("Response returned:")
             print(response)
             return
-        self.next_action_time = time.time() + float(data.get('cooldown'))
-        self.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get('description'), data.get('coordinates'), data.get('elevation'), data.get('terrain'), data.get('items'))
+        self.player.next_action_time = time.time() + float(data.get('cooldown'))
+        self.player.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get('description'), data.get('coordinates'), data.get('elevation'), data.get('terrain'), data.get('items'))
         print("Respose:", data)
 
     def carry(self, item):
         response = requests.post(self.base_url + '/adv/carry/',
-                                 headers={'Authorization': self.token}, json={'name': item})
+                                 headers={'Authorization': self.player.token}, json={'name': item})
         try:
             data = response.json()
         except ValueError:
@@ -215,14 +208,14 @@ class Actions:
             print("Response returned:")
             print(response)
             return
-        self.next_action_time = time.time() + float(data.get('cooldown'))
-        self.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get(
+        self.player.next_action_time = time.time() + float(data.get('cooldown'))
+        self.player.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get(
             'description'), data.get('coordinates'), data.get('elevation'), data.get('terrain'), data.get('items'))
         print("Response:", data)      
 
     def receive(self):
         response = requests.post(self.base_url + '/adv/receive/',
-                                 headers={'Authorization': self.token})
+                                 headers={'Authorization': self.player.token})
         try:
             data = response.json()
         except ValueError:
@@ -230,14 +223,14 @@ class Actions:
             print("Response returned:")
             print(response)
             return
-        self.next_action_time = time.time() + float(data.get('cooldown'))
-        self.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get(
+        self.player.next_action_time = time.time() + float(data.get('cooldown'))
+        self.player.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get(
             'description'), data.get('coordinates'), data.get('elevation'), data.get('terrain'), data.get('items'))
         print("Response:", data)    
 
     def get_balance(self, item):
         response = requests.post(self.base_url + '/adv/get_balance/',
-                                 headers={'Authorization': self.token})
+                                 headers={'Authorization': self.player.token})
         try:
             data = response.json()
         except ValueError:
@@ -245,13 +238,13 @@ class Actions:
             print("Response returned:")
             print(response)
             return
-        self.next_action_time = time.time() + float(data.get('cooldown'))
+        self.player.next_action_time = time.time() + float(data.get('cooldown'))
         self.message = data.get('cooldown')[0]
         print("Response:", data)
 
     def transmogrify(self, item):
         response = requests.post(self.base_url + '/adv/transmogrify/',
-                                 headers={'Authorization': self.token}, json={'name': item})
+                                 headers={'Authorization': self.player.token}, json={'name': item})
         try:
             data = response.json()
         except ValueError:
@@ -259,7 +252,7 @@ class Actions:
             print("Response returned:")
             print(response)
             return
-        self.next_action_time = time.time() + float(data.get('cooldown'))
-        self.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get(
+        self.player.next_action_time = time.time() + float(data.get('cooldown'))
+        self.player.current_room = Room(data.get('room_id'), data.get('exits'), data.get('title'), data.get(
             'description'), data.get('coordinates'), data.get('elevation'), data.get('terrain'), data.get('items'))
         print("Response:", data)                       

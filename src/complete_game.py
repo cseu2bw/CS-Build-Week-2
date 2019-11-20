62615f7fa6943f0a900f130d3de8725e5e13fd4f
AaronJThompson <aaronjthompson98@gmail.com>
2019-11-20 10:35:40 +0000

    Add gold checking

--- src/complete_game.py
+++ src/complete_game.py
@@ -1,6 +1,7 @@
 from game import Game
 from player import Player
 import os
+import math
 
 name = os.environ['PLAYER_NAME']
 
@@ -9,7 +10,9 @@ player = Player(game)
 player.queue_func(player.init)
 player.queue_func(player.actions.check_status)
 if player.status.name != name:
-  player.collect_treasures(10)
-  player.sell_items()
+  target_items = int(math.ceil(((1000 - player.status.gold) / 100)))
+  if target_items > 0:
+    player.collect_treasures(target_items)
+    player.sell_items()
   player.change_name(name)
 player.go_next_block()

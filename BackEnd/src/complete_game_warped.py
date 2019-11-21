445cd1d1bc02cc481b7efbe012c5cdad9286c3a2
AaronJThompson <aaronjthompson98@gmail.com>
2019-11-21 14:46:48 +0000

    Remove init and check_status from complete_game_warped

--- BackEnd/src/complete_game_warped.py
+++ BackEnd/src/complete_game_warped.py
@@ -7,8 +7,6 @@ name = os.environ['PLAYER_NAME']
 
 game = Game()
 player = Player(game)
-player.queue_func(player.init)
-player.queue_func(player.actions.check_status)
 # if player.status.name != name:
 #   target_items = int(math.ceil(((1000 - player.status.gold) / 100)))
 #   if target_items > 0:
